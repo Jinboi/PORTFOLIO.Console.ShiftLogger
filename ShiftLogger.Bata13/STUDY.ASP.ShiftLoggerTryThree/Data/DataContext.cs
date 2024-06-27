07ec14e6d815daa10e567d99395aa0dbdb884852
Youngwoo Jin <duddnwkfgo7@gmail.com>
2024-06-26 20:25:47 -0700

    copied and pasted project for portfolio purposes

---
+++ ShiftLogger.Bata13/STUDY.ASP.ShiftLoggerTryThree/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STUDY.ASP.ShiftLoggerTryThree.Models;
+
+namespace STUDY.ASP.ShiftLoggerTryThree.Data;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+    public DbSet<ShiftLogger> Shifts { get; set; }
+}
